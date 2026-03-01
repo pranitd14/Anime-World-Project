755a48935f96ea2750bcc190c3691cb6cc17f52e
Pranit Deshmukh <pranitdeshmukh2153@gmail.com>
2026-03-01 15:23:55 +0530

    Delete replace_links.bat

--- replace_links.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for %%f in (*.html) do (
-    powershell -Command "(Get-Content '%%f') -replace 'https://mega\\.nz/file/[^\"]*', 'demo.mp4' | Out-File '%%f' -Encoding UTF8"
-)
-echo All video links replaced with demo.mp4
